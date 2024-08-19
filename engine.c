b9cdb5e4a71a309eb91c2e8a27ac26590cebe160
Lotusdelta <nammig@kpit.com>
2024-08-19 17:06:39 +0530

    added Rear Camer Code

--- engine.c
+++ engine.c
@@ -3,7 +3,10 @@ int EngineControl()
 {
 	printf("EngineControl code here");
 }
-
+int RearCamer()
+{
+	printf("RearCamera Code here");
+}
 int main()
 {
 printf("Vehicle Main Component Code will be here");
